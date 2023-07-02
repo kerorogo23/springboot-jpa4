6e0c2dd916c4a849083aecdbdccd11b0cbb05ff4
user <saygood5566@gmail.com>
2023-07-02 21:43:52 +0800

    SpringBoot初始化

---
+++ springboot-jpa4/src/main/java/com/jack/springbootjpa4/SpringbootJpa4Application.java
@@ -0,0 +1,13 @@
+package com.jack.springbootjpa4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpa4Application.class, args);
+	}
+
+}
---
+++ springboot-jpa4/src/test/java/com/jack/springbootjpa4/SpringbootJpa4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.springbootjpa4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpa4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
